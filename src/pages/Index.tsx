99dd6699701802f9d63a8eeff95b0990ecd9bc38
patidarmk <34210081+patidarmk@users.noreply.github.com>
2025-09-09 12:41:24 +0530

    Add src/pages/Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import { TodoList } from "@/components/todo/TodoList";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <TodoList />
+    </div>
+  );
+};
+
+export default Index;
